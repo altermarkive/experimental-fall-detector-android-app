a222147b6d88cbabd1efb4f25349c2e817224d0e
altermarkive <altermarkive@gmail.com>
2021-08-09 13:52:23 +0200

    Combining the sensor sampler code with the rest of the app

--- fall-detector/src/main/java/altermarkive/guardian/Guardian.kt
+++ fall-detector/src/main/java/altermarkive/guardian/Guardian.kt
@@ -14,6 +14,7 @@ class Guardian : Service() {
     override fun onCreate() {
         Positioning.initiate(this)
         Detector.instance(this)
+        Sampler.instance(this)
         Alarm.instance(this)
     }
 
--- fall-detector/src/main/java/altermarkive/guardian/Sampler.kt
+++ fall-detector/src/main/java/altermarkive/guardian/Sampler.kt
@@ -3,11 +3,11 @@ package altermarkive.guardian
 import android.content.Context
 import android.os.PowerManager
 
-class Sampler private constructor(private val context: Context) {
-//    private val config: Config
+class Sampler private constructor(private val guardian: Guardian) {
+    //    private val config: Config
 //    private val data: Data
     fun context(): Context {
-        return context
+        return guardian.applicationContext
     }
 
 //    fun config(): Config {
@@ -18,26 +18,26 @@ class Sampler private constructor(private val context: Context) {
 //        return data
 //    }
 
-    fun initiate(context: Context) {
-        val manager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+    private fun initiate() {
+        val manager = context().getSystemService(Context.POWER_SERVICE) as PowerManager
         val lock = manager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG)
         if (!lock.isHeld) {
             lock.acquire()
         }
-        initiate()
     }
-
-    private external fun initiate()
 
     companion object {
         private val TAG = Sampler::class.java.name
 
         @Volatile
         private var instance: Sampler? = null
+
         @Synchronized
-        fun instance(context: Context): Sampler? {
+        fun instance(guardian: Guardian): Sampler {
+            var instance = this.instance
             if (instance == null) {
-                instance = Sampler(context)
+                instance = Sampler(guardian)
+                this.instance = instance
             }
             return instance
         }
@@ -50,5 +50,6 @@ class Sampler private constructor(private val context: Context) {
     init {
 //        config = Config(this)
 //        data = Data()
+        initiate()
     }
 }
