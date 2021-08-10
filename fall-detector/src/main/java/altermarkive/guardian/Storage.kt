2d3541dc4307415383a6d2c82b9b906b5740a4b7
altermarkive <altermarkive@gmail.com>
2021-08-10 17:40:57 +0200

    Converted storage I/O code from Java to Kotlin

--- fall-detector/src/main/java/altermarkive/guardian/Storage.kt
+++ fall-detector/src/main/java/altermarkive/guardian/Storage.kt
@@ -1,148 +1,140 @@
-package altermarkive.uploader;
+package altermarkive.guardian
 
-import android.os.Environment;
+import android.os.Environment
+import android.util.Log
+import java.io.*
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-public class Storage {
-    private final static String TAG = Storage.class.getName();
-    private final static File PREFIX = prefix();
-
-    private static File prefix() {
-        String self = Storage.class.getPackage().getName();
-        File root = Environment.getExternalStorageDirectory();
-        File directory = new File(root, "Data" + File.separator + self);
+object Storage {
+    private val TAG = Storage::class.java.name
+    private val PREFIX = prefix()
+    private fun prefix(): File? {
+        val self = Storage::class.java.getPackage().name
+        val root = Environment.getExternalStorageDirectory()
+        val directory = File(root, "Data" + File.separator + self)
         if (!directory.exists() && !directory.mkdirs()) {
-            String message = String.format("Failed to make path for '%s'", directory);
-            android.util.Log.d(TAG, message);
-            return null;
+            val message = String.format("Failed to make path for '%s'", directory)
+            Log.d(TAG, message)
+            return null
         }
-        return directory;
+        return directory
     }
 
-    public static long size(String file) {
-        File path = new File(PREFIX, file);
-        return path.length();
+    fun size(file: String?): Long {
+        val path = File(PREFIX, file)
+        return path.length()
     }
 
-    public static String readText(String file) {
-        long size = size(file);
-        if (Integer.MAX_VALUE < size) {
-            android.util.Log.d(TAG, "Cannot read file into memory");
-            return null;
+    fun readText(file: String?): String? {
+        val size = size(file)
+        if (Int.MAX_VALUE < size) {
+            Log.d(TAG, "Cannot read file into memory")
+            return null
         }
-        byte[] buffer = new byte[(int) size];
-        if (readBinary(file, buffer)) {
-            return new String(buffer);
+        val buffer = ByteArray(size.toInt())
+        return if (readBinary(file, buffer)) {
+            String(buffer)
         } else {
-            return null;
+            null
         }
     }
 
-    public static String writeText(String file, String content) {
-        return writeBinary(file, content.getBytes()) ? content : null;
+    fun writeText(file: String?, content: String): String? {
+        return if (writeBinary(file, content.toByteArray())) content else null
     }
 
-    public static String appendText(String file, String content) {
-        return appendBinary(file, content.getBytes()) ? content : null;
+    fun appendText(file: String?, content: String): String? {
+        return if (appendBinary(file, content.toByteArray())) content else null
     }
 
-    public static boolean readBinary(String file, byte[] content) {
-        File path = new File(PREFIX, file);
-        InputStream input;
-        try {
-            input = new FileInputStream(path);
-        } catch (FileNotFoundException exception) {
-            String trace = Log.getStackTraceString(exception);
-            String message = String.format("Failed to open '%s' for reading:\n%s", file, trace);
-            android.util.Log.d(TAG, message);
-            return false;
+    fun readBinary(file: String?, content: ByteArray): Boolean {
+        val path = File(PREFIX, file)
+        val input: InputStream
+        input = try {
+            FileInputStream(path)
+        } catch (exception: FileNotFoundException) {
+            val trace: String = Log.getStackTraceString(exception)
+            val message = String.format("Failed to open '%s' for reading:\n%s", file, trace)
+            Log.d(TAG, message)
+            return false
         }
-        boolean result = true;
+        var result = true
         try {
-            if (input.read(content) < content.length) {
-                String message = String.format("Mismatched buffer size for '%s'", file);
-                android.util.Log.d(TAG, message);
-                result = false;
+            if (input.read(content) < content.size) {
+                val message = String.format("Mismatched buffer size for '%s'", file)
+                Log.d(TAG, message)
+                result = false
             }
-        } catch (IOException exception) {
-            String trace = Log.getStackTraceString(exception);
-            String message = String.format("Failed to read '%s':\n%s", file, trace);
-            android.util.Log.d(TAG, message);
-            result = false;
+        } catch (exception: IOException) {
+            val trace: String = Log.getStackTraceString(exception)
+            val message = String.format("Failed to read '%s':\n%s", file, trace)
+            Log.d(TAG, message)
+            result = false
         }
         try {
-            input.close();
-        } catch (IOException exception) {
-            String trace = Log.getStackTraceString(exception);
-            String message = String.format("Failed to close '%s':\n%s", file, trace);
-            android.util.Log.d(TAG, message);
-            result = false;
+            input.close()
+        } catch (exception: IOException) {
+            val trace: String = Log.getStackTraceString(exception)
+            val message = String.format("Failed to close '%s':\n%s", file, trace)
+            Log.d(TAG, message)
+            result = false
         }
-        return result;
+        return result
     }
 
-    public static boolean writeBinary(String file, byte[] content) {
-        return writeBinary(file, content, false);
+    fun writeBinary(file: String?, content: ByteArray): Boolean {
+        return writeBinary(file, content, false)
     }
 
-    public static boolean appendBinary(String file, byte[] content) {
-        return writeBinary(file, content, true);
+    fun appendBinary(file: String?, content: ByteArray): Boolean {
+        return writeBinary(file, content, true)
     }
 
-    private static boolean writeBinary(String file, byte[] content, boolean append) {
-        File path = new File(PREFIX, file);
-        if (PREFIX.getFreeSpace() < content.length) {
-            String message = String.format("No space left for '%s'", file);
-            android.util.Log.d(TAG, message);
-            return false;
+    private fun writeBinary(file: String?, content: ByteArray, append: Boolean): Boolean {
+        val path = File(PREFIX, file)
+        if (PREFIX!!.freeSpace < content.size) {
+            val message = String.format("No space left for '%s'", file)
+            Log.d(TAG, message)
+            return false
         }
-        OutputStream output;
+        val output: OutputStream
+        output = try {
+            FileOutputStream(path, append)
+        } catch (exception: FileNotFoundException) {
+            val trace: String = Log.getStackTraceString(exception)
+            val message = String.format("Failed to open '%s' for writing:\n%s", file, trace)
+            Log.d(TAG, message)
+            return false
+        }
+        var result = true
         try {
-            output = new FileOutputStream(path, append);
-        } catch (FileNotFoundException exception) {
-            String trace = Log.getStackTraceString(exception);
-            String message = String.format("Failed to open '%s' for writing:\n%s", file, trace);
-            android.util.Log.d(TAG, message);
-            return false;
-        }
-        boolean result = true;
-        try {
-            output.write(content);
-        } catch (IOException exception) {
-            String trace = Log.getStackTraceString(exception);
-            String message = String.format("Failed to write '%s':\n%s", file, trace);
-            android.util.Log.d(TAG, message);
-            result = false;
+            output.write(content)
+        } catch (exception: IOException) {
+            val trace: String = Log.getStackTraceString(exception)
+            val message = String.format("Failed to write '%s':\n%s", file, trace)
+            Log.d(TAG, message)
+            result = false
         }
         try {
-            output.close();
-        } catch (IOException exception) {
-            String trace = Log.getStackTraceString(exception);
-            String message = String.format("Failed to close '%s':\n%s", file, trace);
-            android.util.Log.d(TAG, message);
-            result = false;
+            output.close()
+        } catch (exception: IOException) {
+            val trace: String = Log.getStackTraceString(exception)
+            val message = String.format("Failed to close '%s':\n%s", file, trace)
+            Log.d(TAG, message)
+            result = false
         }
-        return result;
+        return result
     }
 
-    public static boolean delete(String file) {
-        if (!new File(PREFIX, file).delete()) {
-            String message = String.format("Failed to delete '%s'", file);
-            android.util.Log.d(TAG, message);
-            return false;
+    fun delete(file: String?): Boolean {
+        if (!File(PREFIX, file).delete()) {
+            val message = String.format("Failed to delete '%s'", file)
+            Log.d(TAG, message)
+            return false
         }
-        return true;
+        return true
     }
 
-    public static String[] list(FilenameFilter filter) {
-        return PREFIX.list(filter);
+    fun list(filter: FilenameFilter?): Array<String> {
+        return PREFIX!!.list(filter)
     }
 }
