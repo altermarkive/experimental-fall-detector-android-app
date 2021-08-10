37674d5e4b0a75e1efc37f6046501580d5b9cf53
altermarkive <altermarkive@gmail.com>
2021-08-10 17:58:28 +0200

    Adjustment to the storage I/O code after the automatic Javba to Kotlin conversion

--- fall-detector/src/main/java/altermarkive/guardian/Storage.kt
+++ fall-detector/src/main/java/altermarkive/guardian/Storage.kt
@@ -1,55 +1,41 @@
 package altermarkive.guardian
 
-import android.os.Environment
 import android.util.Log
 import java.io.*
 
 object Storage {
     private val TAG = Storage::class.java.name
-    private val PREFIX = prefix()
-    private fun prefix(): File? {
-        val self = Storage::class.java.getPackage().name
-        val root = Environment.getExternalStorageDirectory()
-        val directory = File(root, "Data" + File.separator + self)
-        if (!directory.exists() && !directory.mkdirs()) {
-            val message = String.format("Failed to make path for '%s'", directory)
-            Log.d(TAG, message)
-            return null
-        }
-        return directory
-    }
 
-    fun size(file: String?): Long {
-        val path = File(PREFIX, file)
+    internal fun size(prefix: String, file: String): Long {
+        val path = File(prefix, file)
         return path.length()
     }
 
-    fun readText(file: String?): String? {
-        val size = size(file)
+    internal fun readText(prefix: String, file: String): String? {
+        val size = size(prefix, file)
         if (Int.MAX_VALUE < size) {
             Log.d(TAG, "Cannot read file into memory")
             return null
         }
         val buffer = ByteArray(size.toInt())
-        return if (readBinary(file, buffer)) {
+        return if (readBinary(prefix, file, buffer)) {
             String(buffer)
         } else {
             null
         }
     }
 
-    fun writeText(file: String?, content: String): String? {
-        return if (writeBinary(file, content.toByteArray())) content else null
+    internal fun writeText(prefix: String, file: String, content: String): String? {
+        return if (writeBinary(prefix, file, content.toByteArray())) content else null
     }
 
-    fun appendText(file: String?, content: String): String? {
-        return if (appendBinary(file, content.toByteArray())) content else null
+    internal fun appendText(prefix: String, file: String, content: String): String? {
+        return if (appendBinary(prefix, file, content.toByteArray())) content else null
     }
 
-    fun readBinary(file: String?, content: ByteArray): Boolean {
-        val path = File(PREFIX, file)
-        val input: InputStream
-        input = try {
+    internal fun readBinary(prefix: String, file: String, content: ByteArray): Boolean {
+        val path = File(prefix, file)
+        val input: InputStream = try {
             FileInputStream(path)
         } catch (exception: FileNotFoundException) {
             val trace: String = Log.getStackTraceString(exception)
@@ -81,23 +67,22 @@ object Storage {
         return result
     }
 
-    fun writeBinary(file: String?, content: ByteArray): Boolean {
-        return writeBinary(file, content, false)
+    internal fun writeBinary(prefix: String, file: String, content: ByteArray): Boolean {
+        return writeBinary(prefix, file, content, false)
     }
 
-    fun appendBinary(file: String?, content: ByteArray): Boolean {
-        return writeBinary(file, content, true)
+    internal fun appendBinary(prefix: String, file: String, content: ByteArray): Boolean {
+        return writeBinary(prefix, file, content, true)
     }
 
-    private fun writeBinary(file: String?, content: ByteArray, append: Boolean): Boolean {
-        val path = File(PREFIX, file)
-        if (PREFIX!!.freeSpace < content.size) {
+    private fun writeBinary(prefix: String, file: String, content: ByteArray, append: Boolean): Boolean {
+        val path = File(prefix, file)
+        if (File(prefix).freeSpace < content.size) {
             val message = String.format("No space left for '%s'", file)
             Log.d(TAG, message)
             return false
         }
-        val output: OutputStream
-        output = try {
+        val output: OutputStream = try {
             FileOutputStream(path, append)
         } catch (exception: FileNotFoundException) {
             val trace: String = Log.getStackTraceString(exception)
@@ -125,8 +110,8 @@ object Storage {
         return result
     }
 
-    fun delete(file: String?): Boolean {
-        if (!File(PREFIX, file).delete()) {
+    internal fun delete(prefix: String, file: String): Boolean {
+        if (!File(prefix, file).delete()) {
             val message = String.format("Failed to delete '%s'", file)
             Log.d(TAG, message)
             return false
@@ -134,7 +119,7 @@ object Storage {
         return true
     }
 
-    fun list(filter: FilenameFilter?): Array<String> {
-        return PREFIX!!.list(filter)
+    internal fun list(prefix: String, filter: FilenameFilter): Array<String>? {
+        return File(prefix).list(filter)
     }
 }
