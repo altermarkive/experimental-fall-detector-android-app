bb80286274970ae38aa68e081b89b621b6ec4707
altermarkive <altermarkive@gmail.com>
2021-08-10 10:45:24 +0200

    Converted data aggregation code from Java to Kotlin

--- fall-detector/src/main/java/altermarkive/guardian/Data.kt
+++ fall-detector/src/main/java/altermarkive/guardian/Data.kt
@@ -1,127 +1,134 @@
-package altermarkive.uploader;
+package altermarkive.guardian
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.util.Arrays;
-import java.util.Vector;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.util.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+import kotlin.jvm.Synchronized
+import kotlin.Throws
 
-public class Data implements Runnable {
-    private final static String TAG = Data.class.getName();
-    public final static String MIME = "application/zip";
-
-    private final ByteBuffer bytes = ByteBuffer.allocate(64).order(ByteOrder.LITTLE_ENDIAN);
-    private final Vector<Batch> queue = new Vector<Batch>();
-    private int[] portions = new int[0];
-    private int storing = 0;
-    private long schedule = 0;
-
-    public Data() {
-        new Thread(this).start();
-    }
-
-    public void initiate(int[] sizes, int[] periods, int storing) {
-        this.storing = 0;
+class Data : Runnable {
+    private val bytes = ByteBuffer.allocate(64).order(ByteOrder.LITTLE_ENDIAN)
+    private val queue: Vector<Batch> = Vector<Batch>()
+    private var portions = IntArray(0)
+    private var storing = 0
+    private var schedule: Long = 0
+    fun initiate(sizes: IntArray, periods: IntArray, storing: Int) {
+        this.storing = 0
         // Calculate the batch portions
-        int[] portions = new int[sizes.length];
-        Arrays.fill(portions, 0);
-        for (int i = 0; i < sizes.length; i++) {
+        val portions = IntArray(sizes.size)
+        Arrays.fill(portions, 0)
+        for (i in sizes.indices) {
             if (periods[i] != 0) {
-                portions[i] = sizes[i] * (int) Math.ceil((double) storing / (double) periods[i]);
+                portions[i] = sizes[i] * Math.ceil(
+                    storing.toDouble() / periods[i].toDouble()
+                ).toInt()
             }
         }
         // Initiate the batch queue
-        queue.clear();
-        this.portions = portions;
-        this.storing = storing;
-        advance();
+        queue.clear()
+        this.portions = portions
+        this.storing = storing
+        advance()
     }
 
-    @SuppressWarnings("unused")
-    public synchronized void dispatch(int type, int index, long stamp, float[] values, int axes) {
+    @Synchronized
+    fun dispatch(type: Int, index: Int, stamp: Long, values: FloatArray, axes: Int) {
         if (storing != 0) {
-            bytes.position(0);
-            bytes.putLong(stamp);
-            for (int i = 0; i < axes; i++) {
-                bytes.putFloat(values[i]);
+            bytes.position(0)
+            bytes.putLong(stamp)
+            for (i in 0 until axes) {
+                bytes.putFloat(values[i])
             }
-            Batch batch = find(index);
-            batch.append(index, bytes.array(), bytes.position());
-            synchronized (this) {
-                notifyAll();
-            }
+            val batch: Batch = find(index)
+            batch.append(index, bytes.array(), bytes.position())
+            synchronized(this) { notifyAll() }
         }
     }
 
-    public void run() {
-        //noinspection InfiniteLoopStatement
+    override fun run() {
         while (true) {
-            while (storing != 0 && 1 < queue.size()) {
-                Batch batch = queue.remove(0);
-                zip(batch);
+            while (storing != 0 && 1 < queue.size) {
+                val batch: Batch = queue.removeAt(0)
+                zip(batch)
             }
-            synchronized (this) {
+            synchronized(this) {
                 try {
-                    wait();
-                } catch (InterruptedException exception) {
-                    String trace = Log.getStackTraceString(exception);
-                    String message = String.format("Data thread was interrupted:\n%s", trace);
-                    Log.d(TAG, message);
+                    wait()
+                } catch (exception: InterruptedException) {
+                    val trace: String = Log.getStackTraceString(exception)
+                    val message = String.format("Data thread was interrupted:\n%s", trace)
+                    Log.d(TAG, message)
                 }
             }
         }
     }
 
-    private Batch advance() {
-        Batch batch = new Batch(portions);
-        queue.add(batch);
-        if (schedule == 0) {
-            schedule = System.currentTimeMillis();
+    private fun advance(): Batch {
+        val batch = Batch(portions)
+        queue.add(batch)
+        if (schedule == 0L) {
+            schedule = System.currentTimeMillis()
         }
-        schedule += storing;
-        return batch;
+        schedule += storing.toLong()
+        return batch
     }
 
-    private Batch find(int index) {
-        Batch last = queue.lastElement();
-        if (schedule < System.currentTimeMillis() || last.full(index)) {
-            return advance();
+    private fun find(index: Int): Batch {
+        val last: Batch = queue.lastElement()
+        return if (schedule < System.currentTimeMillis() || last.full(index)) {
+            advance()
         } else {
-            return last;
+            last
         }
     }
 
-    private void zip(Batch batch) {
-        ByteArrayOutputStream zipped = new ByteArrayOutputStream();
-        ZipOutputStream stream = new ZipOutputStream(zipped);
+    private fun zip(batch: Batch) {
+        val zipped = ByteArrayOutputStream()
+        val stream = ZipOutputStream(zipped)
         try {
-            zipTextFile(stream, "device.json");
-            zipTextFile(stream, Log.LOG_FILE);
-            Storage.writeText(Log.LOG_FILE, "");
-            batch.zip(stream);
-            stream.close();
-            byte[] content = zipped.toByteArray();
-            String name = String.format("data.%016X.zip", batch.stamp());
+            zipTextFile(stream, "device.json")
+            zipTextFile(stream, Log.LOG_FILE)
+            Storage.writeText(Log.LOG_FILE, "")
+            batch.zip(stream)
+            stream.close()
+            val content = zipped.toByteArray()
+            val name = java.lang.String.format("data.%016X.zip", batch.stamp())
             if (!Storage.writeBinary(name, content)) {
-                Log.e(TAG, "Failed to write a ZIP file");
+                Log.e(TAG, "Failed to write a ZIP file")
             }
-        } catch (IOException exception) {
-            Log.e(TAG, "Failed to create a ZIP file:\n" + Log.getStackTraceString(exception));
+        } catch (exception: IOException) {
+            Log.e(
+                TAG, """
+     Failed to create a ZIP file:
+     ${Log.getStackTraceString(exception)}
+     """.trimIndent()
+            )
         }
     }
 
-    private void zipTextFile(ZipOutputStream stream, String file) throws IOException {
-        ZipEntry entry = new ZipEntry(file);
-        String content = Storage.readText(file);
+    @Throws(IOException::class)
+    private fun zipTextFile(stream: ZipOutputStream, file: String) {
+        val entry = ZipEntry(file)
+        val content: String = Storage.readText(file)
         if (content != null) {
-            byte[] bytes = content.getBytes();
-            entry.setSize(bytes.length);
-            stream.putNextEntry(entry);
-            stream.write(bytes, 0, bytes.length);
-            stream.closeEntry();
+            val bytes = content.toByteArray()
+            entry.size = bytes.size.toLong()
+            stream.putNextEntry(entry)
+            stream.write(bytes, 0, bytes.size)
+            stream.closeEntry()
         }
+    }
+
+    companion object {
+        private val TAG = Data::class.java.name
+        const val MIME = "application/zip"
+    }
+
+    init {
+        Thread(this).start()
     }
 }
